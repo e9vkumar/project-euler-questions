afe65a3ca5766cb0df4aa8a235ac15d59588fe5e
vkEnine <vkumar@enine.dev>
2024-06-17 14:19:57 +0530

    Question 31: Coin sum solved

--- 031_coin_sum.py
+++ 031_coin_sum.py
@@ -1,18 +1,19 @@
-def coin_sum(coins,sum,i,dp):
-    if sum==0:
-        dp[i][sum] = 1
-        return dp[i][sum]
-    
-    if sum < 0 or i <= 0:
+def coin_sum(coins, sum, n, dp):
+    if sum == 0:
+        dp[n][sum] = 1
+        return dp[n][sum]
+
+    if sum < 0 or n <= 0:
         return 0
-    
-    if dp[i][sum] != -1:
-        return dp[i][sum]
-    
-    return coin_sum(coins,sum-coins[i],i,dp) + coin_sum(coins,sum,i-1,dp)
+
+    if dp[n][sum] != -1:
+        return dp[n][sum]
+
+    return coin_sum(coins, sum - coins[n-1], n, dp) + coin_sum(coins, sum, n - 1, dp)
+
 
 sum = 200
-coins = [1,2,5,10,20,50,100,200]
-dp = [[-1 for i in range(sum)] for j in range(len(coins))]
-print(coin_sum(coins,sum,len(coins)-1,dp))
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+dp = [[-1 for i in range(sum+1)] for j in range(len(coins)+1)]
+print(coin_sum(coins, sum, len(coins), dp))
 # print(len(dp[0]))
